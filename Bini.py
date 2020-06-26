c6420a4c82d723ee146b15f5e727d8704e56f5f0
bini-panda <65950550+bini-panda@users.noreply.github.com>
2020-06-26 10:50:37 +0300

    Update and rename README.md to Bini.py

---
+++ Bini.py
@@ -0,0 +1,41 @@
+print('''╔╦═╦╗╔═╗╔╗─╔═╗╔═╗╔═╦═╗╔═╗
+║║║║║║╦╝║║─║╔╝║║║║║║║║║╦╝
+║║║║║║╩╗║╚╗║╚╗║║║║║║║║║╩╗
+╚═╩═╝╚═╝╚═╝╚═╝╚═╝╚╩═╩╝╚═╝
+─────────────────────────''')
+print('''What Do You Want To Do?
+          1 [*] Hack Telegram
+          2 [*] Delete Users Account
+          3 [*] Convert Contacts & File
+          4 [*] Exit''')
+n=input('Choose Number:')
+if n=='1':
+	print('''By Which Method You Want Hack
+	         1 By Phone Number
+	         2 By usename''')
+b=input('Select Number:')
+if b=='2':
+	print('')
+	print('''Copy This Link And Send
+	(https://da.gtK10bfHftyy)
+To Victim On Telegram And  Reply And Write *keyloged Then Delete The message.
+Then Come Back Here And Write [Done]''')
+u=input('>>>')
+if u=='Done':
+	print('Successfully KeyLoged')
+	y=input('Send Username Of Victim:')
+	print('Connecting to keyloger  ████████████████████████████]100')
+	print('Connecting to user Account.................../\n')
+	print('''╔══╗╔═╗╔═╦╗╔═╗  ╔╗
+╚╗╗║║║║║║║║║╦╝  ║║
+╔╩╝║║║║║║║║║╩╗  ║║
+╚══╝╚═╝╚╩═╝╚═╝  ╠╣
+──────────────  ╚╝''')
+	print('Successfully Hacked!!! \n')
+	print('Telegram Code is > 124576')
+	print('2 Step Verification Code is> banja131478\n')
+o=input('Do You Want To Change 2 Step Verification [y/n]')
+if o=='n':
+	print('ENJOY HACKING!!!')
+
+	
--- README.md
+++
@@ -1,2 +0,0 @@
-# telehack
-Hack telegram with user bots
